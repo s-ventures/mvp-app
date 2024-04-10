f29e6894d75dd04785c6a3e20b777ce420372900
jesus-silva-ext_soon <jesus.silva.ext@soon.es>
2024-04-10 12:12:56 +0200

    fix(daily_banking): :bug: adjusted heights

--- lib/presentation/daily_banking/accounts/list/account_list_sliver_pinned_header.dart
+++ lib/presentation/daily_banking/accounts/list/account_list_sliver_pinned_header.dart
@@ -6,7 +6,6 @@ import 'package:go_router/go_router.dart';
 import 'package:manifiesto_mvp_app/application/daily_banking/accounts/accounts/simplified/simplified_accounts_controller.dart';
 import 'package:manifiesto_mvp_app/domain/accounts/accounts/entities/simplified_account.dart';
 import 'package:manifiesto_mvp_app/domain/core/value_objects.dart';
-import 'package:manifiesto_mvp_app/presentation/daily_banking/accounts/list/widgets/mockup_account_card.dart';
 import 'package:manifiesto_mvp_app/presentation/routing/routes.dart';
 import 'package:ui_kit/ui_kit.dart';
 
@@ -35,88 +34,70 @@ class _AccountListSliverPinnedHeaderState
     );
     final controller = ref.read(simplifiedAccountsControllerProvider.notifier);
 
-    final height = MeasureUtil.measureWidget(
+    final heightAmount = MeasureUtil.measureWidget(
       Directionality(
         textDirection: TextDirection.ltr,
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                AppSpacing.horizontal.s7,
-                Text(
+        child: SizedBox(
+          height: AppSpacing.s7,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              AppSpacing.horizontal.s7,
+              SizedBox(
+                child: Text(
                   0.00.toCurrency(plusSign: false),
                   style: context.textStyle.h4,
                 ),
-                AppSpacing.horizontal.s7,
-              ],
-            ),
-            AppSpacing.vertical.s2,
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Saldo disponible ',
-                  style: context.textStyle.bodySmallRegular.copyWith(
-                    color: context.color.textLight600,
-                  ),
-                ),
-                Text(
-                  0.00.toCurrency(plusSign: false),
-                  style: context.textStyle.bodySmallSemiBold.copyWith(
-                    color: 0.00 < 0
-                        ? context.color.statusError
-                        : context.color.textLight600,
-                  ),
-                ),
-              ],
-            ),
-            AppSpacing.vertical.s5,
-            SizedBox(
-              height: AppSpacing.s7,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CustomChip(
-                    size: CustomChipSize.extraSmall,
-                    backgroundColor: context.color.backgroundLight200,
-                    leadingIcon: '2103' == '2103' ? IconAssets.soon : null,
-                    leadingIconColor: '2103' == '2103'
-                        ? context.color.secondaryLight600
-                        : null,
-                    title: Row(
-                      children: [
-                        Text(
-                          'Cuenta ',
-                          style: context.textStyle.bodySmallRegular.copyWith(
-                            color: context.color.primaryLight600,
-                          ),
-                        ),
-                        Text(
-                          '2103' == '2103' ? 'soon' : '2103',
-                          style: context.textStyle.bodySmallSemiBold.copyWith(
-                            color: context.color.primaryLight600,
-                          ),
-                        ),
-                        Text(
-                          ' • EUR',
-                          style: context.textStyle.buttonTabBar.copyWith(
-                            color: context.color.primaryLight300,
-                          ),
-                        ),
-                      ],
-                    ),
-                    onSelected: (_) {},
-                  ),
-                ],
               ),
-            ),
-          ],
+              AppSpacing.horizontal.s7,
+            ],
+          ),
         ),
       ),
     ).height;
 
-    debugPrint('height: $height');
+    final heightBalance = MeasureUtil.measureWidget(
+      Directionality(
+        textDirection: TextDirection.ltr,
+        child: SizedBox(
+          height: AppSpacing.s6,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Saldo disponible ',
+                style: context.textStyle.bodySmallRegular.copyWith(
+                  color: context.color.textLight600,
+                ),
+              ),
+              Text(
+                0.00.toCurrency(plusSign: false),
+                style: context.textStyle.bodySmallSemiBold.copyWith(
+                  color: 0.00 < 0
+                      ? context.color.statusError
+                      : context.color.textLight600,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ).height;
+
+    final heightChip = MeasureUtil.measureWidget(
+      Directionality(
+        textDirection: TextDirection.ltr,
+        child: SizedBox(
+          height: AppSpacing.s7,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AppSpacing.vertical.s7,
+            ],
+          ),
+        ),
+      ),
+    ).height;
 
     return SliverAppBar(
       shadowColor: Colors.grey,
@@ -124,7 +105,11 @@ class _AccountListSliverPinnedHeaderState
       pinned: true,
       surfaceTintColor: context.color.backgroundLight200,
       backgroundColor: context.color.backgroundLight200,
-      toolbarHeight: height,
+      toolbarHeight: heightAmount +
+          AppSpacing.s2 +
+          heightBalance +
+          AppSpacing.s5 +
+          heightChip,
       flexibleSpace: accounts.mapOrNull(
             data: (data) => _AccountList(
               accounts: data.value,
@@ -170,93 +155,99 @@ class _AccountList extends StatelessWidget {
           ),
           child: Column(
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  AppSpacing.horizontal.s7,
-                  Text(
-                    account.balance.balance.toCurrency(plusSign: false),
-                    style: context.textStyle.h4,
-                  ),
-                  CustomPopupMenuButton(
-                    items: [
-                      PopupMenuItem(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            const Text('Transferencias programadas'),
-                            AppSpacing.horizontal.s6,
-                            IconSvg.small(IconAssets.calendar),
-                          ],
+              SizedBox(
+                height: AppSpacing.s7,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    AppSpacing.horizontal.s7,
+                    Text(
+                      account.balance.balance.toCurrency(plusSign: false),
+                      style: context.textStyle.h4,
+                    ),
+                    CustomPopupMenuButton(
+                      items: [
+                        PopupMenuItem(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text('Transferencias programadas'),
+                              AppSpacing.horizontal.s6,
+                              IconSvg.small(IconAssets.calendar),
+                            ],
+                          ),
+                          onTap: () => context.pushNamed(
+                            AppRoute.dailyBankingScheduledTransfers.name,
+                          ),
                         ),
-                        onTap: () => context.pushNamed(
-                          AppRoute.dailyBankingScheduledTransfers.name,
+                        PopupMenuItem(
+                          onTap: () => context.pushNamed(
+                            AppRoute.dailyBankingAccountDetails.name,
+                            pathParameters: {
+                              'accountId': account.id.getOrCrash(),
+                            },
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text('Ver detalles'),
+                              AppSpacing.horizontal.s6,
+                              IconSvg.small(IconAssets.bank),
+                            ],
+                          ),
                         ),
-                      ),
-                      PopupMenuItem(
-                        onTap: () => context.pushNamed(
-                          AppRoute.dailyBankingAccountDetails.name,
-                          pathParameters: {
-                            'accountId': account.id.getOrCrash(),
-                          },
+                        PopupMenuItem(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text('Pagar un impuesto'),
+                              AppSpacing.horizontal.s6,
+                              IconSvg.small(IconAssets.invoice),
+                            ],
+                          ),
                         ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            const Text('Ver detalles'),
-                            AppSpacing.horizontal.s6,
-                            IconSvg.small(IconAssets.bank),
-                          ],
+                        PopupMenuItem(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text('Certificados y documentos'),
+                              AppSpacing.horizontal.s6,
+                              IconSvg.small(IconAssets.document),
+                            ],
+                          ),
                         ),
-                      ),
-                      PopupMenuItem(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            const Text('Pagar un impuesto'),
-                            AppSpacing.horizontal.s6,
-                            IconSvg.small(IconAssets.invoice),
-                          ],
-                        ),
-                      ),
-                      PopupMenuItem(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            const Text('Certificados y documentos'),
-                            AppSpacing.horizontal.s6,
-                            IconSvg.small(IconAssets.document),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                  ],
+                ),
               ),
               AppSpacing.vertical.s2,
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Saldo disponible ',
-                    style: context.textStyle.bodySmallRegular.copyWith(
-                      color: context.color.textLight600,
+              SizedBox(
+                height: AppSpacing.s6,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Saldo disponible ',
+                      style: context.textStyle.bodySmallRegular.copyWith(
+                        color: context.color.textLight600,
+                      ),
                     ),
-                  ),
-                  Text(
-                    account.balance.availableBalance
-                        .toCurrency(plusSign: false),
-                    style: context.textStyle.bodySmallSemiBold.copyWith(
-                      color: account.balance.availableBalance < 0
-                          ? context.color.statusError
-                          : context.color.textLight600,
+                    Text(
+                      account.balance.availableBalance
+                          .toCurrency(plusSign: false),
+                      style: context.textStyle.bodySmallSemiBold.copyWith(
+                        color: account.balance.availableBalance < 0
+                            ? context.color.statusError
+                            : context.color.textLight600,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
               AppSpacing.vertical.s5,
               SizedBox(
-                height: AppSpacing.s7,
+                height: 34,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
