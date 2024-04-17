1d57a9b72fc74f12d7aad0dc4f23ad5cc63a97b9
Jesús  Silva <jesus.silva.ext@soon.es>
2024-04-17 17:59:43 +0200

    refactor(ui): :lipstick: removed useless columns

--- lib/presentation/daily_banking/accounts/list/account_list_page.dart
+++ lib/presentation/daily_banking/accounts/list/account_list_page.dart
@@ -2,59 +2,99 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:ui_kit/ui_kit.dart';
 
-// Todo: Revisar Columns
 class AccountListPage extends StatelessWidget {
   const AccountListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: NestedScrollView(
-          headerSliverBuilder: (context, value) {
-            return [
-              CustomAppBar(
-                centerTitle: true,
-                title: 'Cuentas',
-                leading: Button(
-                  icon: IconAssets.arrowLeft,
-                  type: ButtonType.outlined,
-                  size: ButtonSize.extraSmall,
-                  onPressed: () async => context.pop(),
-                ),
-                actions: [
-                  Button(
-                    icon: IconAssets.plus,
-                    size: ButtonSize.extraSmall,
-                    onPressed: () async {},
-                  ),
-                ],
+      body: NestedScrollView(
+        headerSliverBuilder: (context, value) {
+          return [
+            CustomAppBar(
+              centerTitle: true,
+              title: 'Cuentas',
+              leading: Button(
+                icon: IconAssets.arrowLeft,
+                type: ButtonType.outlined,
+                size: ButtonSize.extraSmall,
+                onPressed: () async => context.pop(),
               ),
-            ];
-          },
-          body: Column(
-            children: [
-              Expanded(
-                child: ListView(
-                  padding: const EdgeInsets.all(16),
-                  children: [
-                    CustomCard(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+              actions: [
+                Button(
+                  icon: IconAssets.plus,
+                  size: ButtonSize.extraSmall,
+                  onPressed: () async {},
+                ),
+              ],
+            ),
+          ];
+        },
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView(
+                padding: const EdgeInsets.all(16),
+                children: [
+                  CustomCard(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            IconWithContainer(
+                              icon: IconAssets.euro,
+                              backgroundColor: context.color.neutralLight100,
+                            ),
+                            AppSpacing.horizontal.s3,
+                            Text(
+                              'Total',
+                              style:
+                                  context.textStyle.bodySmallRegular.copyWith(
+                                color: context.color.textLight900,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Text(
+                          789.00.toCurrency(plusSign: false),
+                          style: context.textStyle.bodySmallRegular.copyWith(
+                            color: context.color.textLight900,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  AppSpacing.vertical.s6,
+                  Text(
+                    'Cuentas',
+                    style: context.textStyle.bodyMediumSemiBold.copyWith(
+                      color: context.color.textLight600,
+                    ),
+                  ),
+                  AppSpacing.vertical.s3,
+                  OutlinedList(
+                    children: [
+                      Splash(
+                        borderRadius:
+                            BorderRadius.circular(context.radius.soft),
+                        child: Padding(
+                          padding: const EdgeInsets.all(AppSpacing.s5),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
                               Row(
                                 children: [
                                   IconWithContainer(
-                                    icon: IconAssets.euro,
+                                    icon: IconAssets.soon,
                                     backgroundColor:
-                                        context.color.neutralLight100,
+                                        context.color.secondaryLight600,
+                                    foreground: context.color.neutralLight0,
+                                    subIcon: IconAssets.check,
                                   ),
                                   AppSpacing.horizontal.s3,
                                   Text(
-                                    'Total',
+                                    'soon',
                                     style: context.textStyle.bodySmallRegular
                                         .copyWith(
                                       color: context.color.textLight900,
@@ -62,13 +102,8 @@ class AccountListPage extends StatelessWidget {
                                   ),
                                 ],
                               ),
-                            ],
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
                               Text(
-                                789.00.toCurrency(plusSign: false),
+                                456.00.toCurrency(plusSign: false),
                                 style:
                                     context.textStyle.bodySmallRegular.copyWith(
                                   color: context.color.textLight900,
@@ -76,138 +111,65 @@ class AccountListPage extends StatelessWidget {
                               ),
                             ],
                           ),
-                        ],
+                        ),
                       ),
-                    ),
-                    AppSpacing.vertical.s6,
-                    Text(
-                      'Cuentas',
-                      style: context.textStyle.bodyMediumSemiBold.copyWith(
-                        color: context.color.textLight600,
-                      ),
-                    ),
-                    AppSpacing.vertical.s3,
-                    OutlinedList(
-                      children: [
-                        Splash(
-                          borderRadius:
-                              BorderRadius.circular(context.radius.soft),
-                          child: Padding(
-                            padding: const EdgeInsets.all(AppSpacing.s5),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Row(
-                                      children: [
-                                        IconWithContainer(
-                                          icon: IconAssets.soon,
-                                          backgroundColor:
-                                              context.color.secondaryLight600,
-                                          foreground:
-                                              context.color.neutralLight0,
-                                          subIcon: IconAssets.check,
-                                        ),
-                                        AppSpacing.horizontal.s3,
-                                        Text(
-                                          'soon',
-                                          style: context
-                                              .textStyle.bodySmallRegular
-                                              .copyWith(
-                                            color: context.color.textLight900,
-                                          ),
-                                        ),
-                                      ],
+                    ],
+                  ),
+                  AppSpacing.vertical.s3,
+                  OutlinedList(
+                    children: [
+                      Splash(
+                        borderRadius:
+                            BorderRadius.circular(context.radius.soft),
+                        child: Padding(
+                          padding: const EdgeInsets.all(AppSpacing.s5),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  IconWithContainer(
+                                    icon: IconAssets.santander,
+                                    backgroundColor: context.color.statusError,
+                                    foreground: context.color.neutralLight0,
+                                  ),
+                                  AppSpacing.horizontal.s3,
+                                  Text(
+                                    'Santander',
+                                    style: context.textStyle.bodySmallRegular
+                                        .copyWith(
+                                      color: context.color.textLight900,
                                     ),
-                                  ],
+                                  ),
+                                ],
+                              ),
+                              Text(
+                                333.00.toCurrency(plusSign: false),
+                                style:
+                                    context.textStyle.bodySmallRegular.copyWith(
+                                  color: context.color.textLight900,
                                 ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      456.00.toCurrency(plusSign: false),
-                                      style: context.textStyle.bodySmallRegular
-                                          .copyWith(
-                                        color: context.color.textLight900,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
+                              ),
+                            ],
                           ),
                         ),
-                      ],
-                    ),
-                    AppSpacing.vertical.s3,
-                    OutlinedList(
-                      children: [
-                        Splash(
-                          borderRadius:
-                              BorderRadius.circular(context.radius.soft),
-                          child: Padding(
-                            padding: const EdgeInsets.all(AppSpacing.s5),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Row(
-                                      children: [
-                                        IconWithContainer(
-                                          icon: IconAssets.santander,
-                                          backgroundColor:
-                                              context.color.statusError,
-                                          foreground:
-                                              context.color.neutralLight0,
-                                        ),
-                                        AppSpacing.horizontal.s3,
-                                        Text(
-                                          'Santander',
-                                          style: context
-                                              .textStyle.bodySmallRegular
-                                              .copyWith(
-                                            color: context.color.textLight900,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      333.00.toCurrency(plusSign: false),
-                                      style: context.textStyle.bodySmallRegular
-                                          .copyWith(
-                                        color: context.color.textLight900,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: Button(
-                  title: 'Seleccionar cuenta',
-                  size: ButtonSize.small,
-                  expand: true,
-                  onPressed: () async => context.pop(),
-                ),
-              ),
-            ],
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Button(
+            title: 'Seleccionar cuenta',
+            size: ButtonSize.small,
+            expand: true,
+            onPressed: () async => context.pop(),
           ),
         ),
       ),
