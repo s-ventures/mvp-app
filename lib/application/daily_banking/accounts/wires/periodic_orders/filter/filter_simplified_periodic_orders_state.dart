de2c6a49787a7d3137b6f98cbabf16961bf182a3
Georgeta Canisag <georget.canisag.ext@soon.es>
2024-05-02 13:31:02 +0200

    feat(daily_banking/periodic_orders): ✨ updated application layer for periodic orders filter

--- lib/application/daily_banking/accounts/wires/periodic_orders/filter/filter_simplified_periodic_orders_state.dart
+++ lib/application/daily_banking/accounts/wires/periodic_orders/filter/filter_simplified_periodic_orders_state.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:manifiesto_mvp_app/domain/wires/periodic_orders/entities/periodic_order_frecuency_type.dart';
 
 part 'filter_simplified_periodic_orders_state.freezed.dart';
 
@@ -14,7 +14,8 @@ class FilterSimplifiedPeriodicOrdersState
   const factory FilterSimplifiedPeriodicOrdersState({
     DateTime? startDate,
     DateTime? endDate,
-    RangeValues? amountRange,
-    FilterSimplifiedPeriodicOrdersType? type,
+    double? amountFrom,
+    double? amountTo,
+    PeriodicOrderFrecuencyType? frecuency,
   }) = _FilterSimplifiedPeriodicOrdersState;
 }
