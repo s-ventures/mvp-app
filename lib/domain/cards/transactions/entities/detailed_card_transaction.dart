9d1757d7db191378559e69248bb32efc98cce19f
Georgeta Canisag <georget.canisag.ext@soon.es>
2024-05-23 10:55:29 +0200

    refactor(daily_banking): ♻️ remove unnecesary duplicated files

--- lib/domain/cards/transactions/entities/detailed_card_transaction.dart
+++
@@ -1,8 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'detailed_card_transaction.freezed.dart';
-
-@freezed
-class DetailedCardTransaction with _$DetailedCardTransaction {
-  const factory DetailedCardTransaction() = _DetailedCardTransaction;
-}
